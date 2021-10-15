13ff7db55c82ed6b19da5a684ebcf786145daff8
Robson <robson.miranda@lunes.io>
2021-10-15 15:13:32 -0300

    Fazendo teste de entrada e saida de dados

---
+++ aula01.py
@@ -0,0 +1,6 @@
+# Tudo que o input pega do terminal vem como string
+nome = input("insira seu nome: ")
+print(f"Oi {nome}")
+
+idade = int(input("Insira sua idade: "))
+print(f"Sua idade é {idade}")
